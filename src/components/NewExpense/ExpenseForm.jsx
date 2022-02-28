9a707a48c9dfae3b746d5158e1a63cf4271da33e
kaushalluffa <webdevelopment852@gmail.com>
2022-02-27 22:23:39 -0500

    added chart

--- src/components/NewExpense/ExpenseForm.jsx
+++ src/components/NewExpense/ExpenseForm.jsx
@@ -24,7 +24,7 @@ const ExpenseForm = (props) => {
 
         const expenseData = {
             title: enteredTitle,
-            amount: enteredAmount,
+            amount: +enteredAmount,
             date: new Date(enteredDate)
         }
         props.onSaveExpenseData(expenseData)
@@ -51,6 +51,7 @@ const ExpenseForm = (props) => {
             </div>
         </div>
         <div className="new-expense__actions">
+        <button type="button" onClick={props.onCancel}>Cancel</button>
             <button type='submit'> Add Expense</button>
         </div>
     </form>
